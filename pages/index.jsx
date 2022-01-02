2e5bde688545d9943a3389caa869b4760baf123d
Daniel Yahalom <daniel.yahalom@gmail.com>
2022-01-02 00:13:51 -0600

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+class HomePage extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return(
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default HomePage;
